a0dbe03adc2957558e5d791ee18305958ef8c1ed
Joonwoo Kim <kkn8010@gmail.com>
2022-08-23 14:36:59 +0900

    study: 프로그래머스 코딩테스트 level 1 학습

---
+++ programmers/Solution23.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class Solution23 {
+    public String solution(String[] seoul) {
+//        String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아,
+//        "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요.
+//        seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+        //ex) ["jane", "Kim"]에서 kim 찾기
+        String answer = "";
+        for (int i = 0; i < seoul.length; i++) {
+            if(seoul[i].equals("Kim")){
+                answer = "김서방은 " + i +"에 있다.";
+            }
+        }
+        return answer;
+    }
+}
